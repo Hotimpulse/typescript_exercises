b0836ac143d3e58d0949db1a7ddbb45667fcbb6c
Hotimpulse <hotimpulse@hotmail.com>
2025-11-17 18:39:54 +0300

    docs: typeof, keyof

--- src/components/TypeScript/Opertators.ts
+++ src/components/TypeScript/Opertators.ts
@@ -7,4 +7,28 @@ const obj = {
 
 type Person = typeof obj;
 
+let color = "red" as const; // if we were to use const, then we'd get a type error
+
+type RedColor = typeof color;
+
+const green: RedColor = "green";
+
+function getData(user: Person): number {
+  return 5;
+}
+
+type GetDataFn = typeof getData;
+
+type GetDataReturnValue = ReturnType<typeof getData>; // gets the number return type from getData
+
+type GetDataParamsValue = Parameters<typeof getData>; // gets the number return type from getData
+
 // keyof
+
+type PersonKey = keyof typeof obj;
+
+function getByKey<T, K extends keyof T>(obj: T, key: K): T[K] {
+  return obj[key];
+}
+
+const num = getByKey(obj, "age");
